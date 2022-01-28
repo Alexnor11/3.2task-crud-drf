de0c4d8e4f26876da7e22ca830b952356f02f199
Alexnor11 <advance114@gmail.com>
2022-01-28 22:09:28 +0300

    password

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
         'USER': 'alexnor11',
-        'PASSWORD': 'alexnor$',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
